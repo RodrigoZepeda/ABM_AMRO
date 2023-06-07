e79ebf14caf2175251d1359d5c354e611400fd3c
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2023-06-07 15:29:12 -0400

    added requirements

--- progress_patients_nec.py
+++ progress_patients_nec.py
@@ -1,5 +1,6 @@
 import numpy as np
-#FIXME Nothing works
+
+
 def progress_patients_nec(days, p_pre, vars):
     # Progressing the status of E ensembles by one day
     # No environmental contamination
@@ -8,41 +9,44 @@ def progress_patients_nec(days, p_pre, vars):
     pos = 0
     new = 0
     imp = 0
-    active_P = days[min(days.keys())]["Patients"]
-    active_W = days[min(days.keys())]["Wards"]
+    p_status = p_pre
+    active_p = days[0]["Patients"]
+    active_w = days[0]["Wards"]
 
     for d in range(1, len(days)):
-        P_status = p_pre * (1 - np.tile(vars["Alpha"], (p_pre.shape[0], 1)))
 
-        for k in range(active_W.shape[0]):
-            temp = active_P["ward"] == active_W["ward"][k]
-            X = np.sum(
-                p_pre[active_P["MRN"][temp], :] * np.tile(active_P["weight"][temp], (1, p_pre.shape[1])),
-                axis=0,
-            )
-            X = X / active_W["Size"][k]  # X = sum(Ck) / (nrk)
+        p_status = p_pre * (1 - np.tile(vars["Alpha"], (p_pre.shape[0], 1)))
 
-            temp2 = active_P["MRN"][temp]
+        for k in range(active_w.shape[0]):
+            temp = active_p["ward"] == active_w["ward"][k]
+
+            prod = p_pre[active_p["MRN"][temp], :] * np.transpose(
+                np.tile(active_p["weight"][temp], (p_pre.shape[1], 1)))
+            X = np.sum(prod, axis=0)
+            X = X / active_w["Size"][k]
+
+            temp2 = active_p["MRN"][temp]
             temp3 = vars["Beta"] * X  # Beta * X + eps
             temp3 = np.tile(temp3, (len(temp2), 1)) * (1 - p_pre[temp2, :])
-            P_status[temp2, :] = P_status[temp2, :] + temp3
+            p_status[temp2, :] = p_status[temp2, :] + temp3
 
-        active_P = days[d]["patients"]
-        active_W = days[d]["wards"]
+        active_p = days[d]["Patients"]
+        active_w = days[d]["Wards"]
         new_comer = days[d]["Pfirst"]
-        P_status[new_comer, :] = np.tile(vars["Gamma"], (len(new_comer), 1))
+        # FIXME: Change from when Pfirst is created
+        p_status[new_comer.astype(int), :] = np.tile(vars["Gamma"], (len(new_comer), 1))
 
-        temp = np.where(P_status == 0)
-        P_status[temp] = np.random.rand(len(temp[0])) <= P_status[temp]
+        temp = np.where(p_status != 0)
+        p_status[temp] = np.random.rand(len(temp[0])) <= p_status[temp]
+        new += np.sum(p_status > p_pre)
+        # FIXME: Change from when Pfirst is created
+        imp += np.sum(p_status[new_comer.astype(int), :])
 
-        new += np.sum(P_status > p_pre)
-        imp += np.sum(P_status[new_comer, :])
+        p_pre = p_status
 
-        p_pre = P_status
-
-        temp = P_status[active_P["MRN"], :]
+        temp = p_status[active_p["MRN"], :]
         test = np.tile(vars["Rho"], (temp.shape[0], 1))
         test = test > np.random.rand(*test.shape)
         pos += np.sum(temp * test, axis=0)
 
-    return P_status, pos, new, imp
+    return p_status, pos, new, imp
