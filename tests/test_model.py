c058585ad2767e396cf978322dfaf13d74435233
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2023-08-04 04:07:54 -0400

    fixed test

--- tests/test_model.py
+++ tests/test_model.py
@@ -16,15 +16,14 @@ def test_gamma_equals_1_all_arrivals_infected():
         [52, 53, 17, 200, 87, 99],  # MRN
         [1, 1, 0, 0, 1, 0],  # New_arrival
         [1, 1, 1, 1, 1, 1],  # Weight
-        [0, 0, 0, 0, 0, 0],  # Colonized status under model 1
+        [-1, -1, -1, -1, -1, -1],  # Next day
     ]).transpose()
     parameters = numpy.array([
         [0.1],  # Alpha
         [0.2],  # Beta
         [1.0],  # Gamma
     ]).transpose()
-    model_run = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters,
-                                                          10)
+    model_run = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters,10)
     assert numpy.all(model_run[:, 3] == model_run[:, 6])
 
 def test_sum_of_positives():
@@ -87,19 +86,19 @@ def test_seed_equal():
         [1],
         [20],
     ]).transpose()
-    initial_colonized = numpy.array([[0, 0, 0, 0, 0, 0]]).transpose()
+    initial_colonized = numpy.array([[0, 1, 0, 0, 0, 0]]).transpose()
     ward_matrix = numpy.array([
-        [0, 0, 0, 0, 0, 0],  # Day
-        [1, 1, 1, 1, 1, 1],  # Ward
-        [52, 53, 17, 200, 87, 99],  # MRN
-        [1, 1, 0, 0, 1, 0],  # New_arrival
-        [1, 1, 1, 1, 1, 1],  # Weight
-        [0, 0, 0, 0, 0, 0],  # Colonized status under model 1
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1],  # Day
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # Ward
+        [52, 53, 17, 200, 87, 99, 52, 53, 17, 20],  # MRN
+        [1, 1, 0, 0, 1, 0, 0, 1, 1, 0],  # New_arrival
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # Weight
+        [6, 7, 8, -1, -1, -1, -1, -1, -1, 1],  # Next day position
     ]).transpose()
     parameters = numpy.array([
         [0.1],  # Alpha
         [0.2],  # Beta
-        [0.01],  # Gamma
+        [0.5],  # Gamma
     ]).transpose()
     model_1 = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters, 10)
     model_2 = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters, 10)
@@ -111,20 +110,20 @@ def test_seed_different():
         [1],
         [20],
     ]).transpose()
-    initial_colonized = numpy.array([[0, 0, 0, 0, 0, 0]]).transpose()
+    initial_colonized = numpy.array([[0, 1, 0, 0, 0, 0]]).transpose()
     ward_matrix = numpy.array([
-        [0, 0, 0, 0, 0, 0],  # Day
-        [1, 1, 1, 1, 1, 1],  # Ward
-        [52, 53, 17, 200, 87, 99],  # MRN
-        [1, 1, 0, 0, 1, 0],  # New_arrival
-        [1, 1, 1, 1, 1, 1],  # Weight
-        [0, 0, 0, 0, 0, 0],  # Colonized status under model 1
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1],  # Day
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # Ward
+        [52, 53, 17, 200, 87, 99, 52, 53, 17, 20],  # MRN
+        [1, 1, 0, 0, 1, 0, 0, 1, 1, 0],  # New_arrival
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # Weight
+        [6, 7, 8, -1, -1, -1, -1, -1, -1, 1],  # Next day position
     ]).transpose()
     parameters = numpy.array([
         [0.1],  # Alpha
-        [0.2],  # Beta
-        [0.01],  # Gamma
+        [1.0],  # Beta
+        [0.5],  # Gamma
     ]).transpose()
     model_1 = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters, 1)
-    model_2 = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters, 2)
+    model_2 = amro.simulate_discrete_model_internal_one(initial_colonized, ward_matrix, total_patients, parameters, 10)
     assert ~numpy.all(model_1 == model_2)
